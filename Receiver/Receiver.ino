753883b181a5eca956e01f80d4acf0920b1048b4
Lukas <luksablp@gmail.com>
2018-01-27 14:33:29 +0100

    init

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,74 @@
+// Author Teemu Mäntykallio, 2017-04-07
+
+// Define pins
+#define DIR_PIN1   13
+#define STEP_PIN1  12 															// Step on rising edge
+#define DIR_PIN2   14
+#define STEP_PIN2  27                               // Step on rising edge
+
+HardwareSerial Serial2(2);
+
+#include <SPI.h>
+#include "RF24.h"
+
+#include <TMC2208Stepper.h>												// Include library
+TMC2208Stepper driver = TMC2208Stepper(&Serial2);	// Create driver and use
+// HardwareSerial0 for communication
+
+RF24 radio(21, 22);
+byte addresses[][6] = {"1Node", "2Node"};
+
+void setup() {
+  Serial.begin(115200);														// Init used serial port
+  while (!Serial);																	// Wait for port to be ready
+
+  Serial.println("Hi!");
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW); // LOW power
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
+  radio.startListening();
+
+  Serial2.begin(115200);                            // Init used serial port
+  while (!Serial2);
+
+  // Prepare pins
+  pinMode(DIR_PIN1, OUTPUT);
+  pinMode(STEP_PIN1, OUTPUT);
+  pinMode(DIR_PIN2, OUTPUT);
+  pinMode(STEP_PIN2, OUTPUT);
+
+  driver.pdn_disable(1);													// Use PDN/UART pin for communication
+  driver.I_scale_analog(0);												// Adjust current from the registers
+  driver.rms_current(500);												// Set driver current 500mA
+  driver.toff(0x2);																// Enable driver
+  driver.microsteps(256);
+  driver.mstep_reg_select(1);
+  driver.en_spreadCycle(false);
+
+  digitalWrite(DIR_PIN1, LOW);
+  digitalWrite(DIR_PIN2, LOW);
+}
+
+void loop() {
+  char cha[] = "";
+  if ( radio.available()) {
+    // Variable for the received timestamp
+    while (radio.available()) {                                   // While there is data ready
+      radio.read( &cha, radio.getDynamicPayloadSize() );             // Get the payload
+    }
+
+    radio.stopListening();                                        // First, stop listening so we can talk
+    radio.write( &cha, strlen(cha) );              // Send the final one back.
+    radio.startListening();
+  }
+  if (strlen(cha) > 0)
+    Serial.println(cha);
+
+  /*for (int i = 0; i < 102040; i++) { //256*200*2
+    digitalWrite(STEP_PIN1, !digitalRead(STEP_PIN1)); // Step
+    delayMicroseconds(25);
+    }
+    digitalWrite(DIR_PIN1, !digitalRead(DIR_PIN1)); // Rotate*/
+}
---
+++ Sender/Sender.ino
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include "RF24.h"
+RF24 radio(21, 22);
+byte addresses[][6] = {"1Node", "2Node"};
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1, addresses[0]);
+}
+
+void loop() {
+  while (Serial.available() == 0); // wait for Serial
+  char endMarker = '\n';
+  String str = Serial.readStringUntil(endMarker);
+  //Serial.println(getValue(str, ' ', 0));
+
+  char* cha = 0;
+  if (cha != 0) {
+    delete [] cha;
+  }
+  cha = new char [str.length()];
+  str.toCharArray(cha, str.length());
+  Serial.println(cha);
+  radio.stopListening();
+
+  unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
+  if (!radio.write( &cha, strlen(cha) )) {
+    Serial.println(F("failed"));
+  }
+  radio.startListening();                                    // Now, continue listening
+
+  unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+  boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+
+  while ( ! radio.available() ) {                            // While nothing is received
+    if (micros() - started_waiting_at > 200000 ) {           // If waited longer than 200ms, indicate timeout and exit while loop
+      timeout = true;
+      break;
+    }
+  }
+
+  if ( timeout ) {                                            // Describe the results
+    Serial.println(F("Failed, response timed out."));
+  } else {
+    char got_time[] = "";      // Grab the response, compare, and send to debugging spew
+    radio.read( &got_time, radio.getDynamicPayloadSize() );
+    unsigned long end_time = micros();
+
+    // Spew it
+    Serial.print(F("Sent "));
+    Serial.print(start_time);
+    Serial.print(F(", Got response "));
+    Serial.print(got_time);
+    Serial.print(F(", Round-trip delay "));
+    Serial.print(end_time - start_time);
+    Serial.println(F(" microseconds"));
+  }
+  delay(100);
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
